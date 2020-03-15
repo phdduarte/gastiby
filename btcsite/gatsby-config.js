deb02982625b9b6b8f6ccefd0a65875c60d530e3
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-03-15 19:02:25 -0300

    Add plugin manifest para o plugin offline

--- btcsite/gatsby-config.js
+++ btcsite/gatsby-config.js
@@ -59,6 +59,17 @@ module.exports = {
                     }
                 ],
             }
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `GatsbyJS`,
+                short_name: `GatsbyJS`,
+                start_url: `/`,
+                background_color: `#f7f0eb`,
+                theme_color: `#a2466c`,
+                display: `standalone`,
+            },
         }
     ]
 }
