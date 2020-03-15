4d98fe8cebd856dd0179c99b087654a83681651a
Pedro Duarte <pedro.duarte@toroinvestimentos.com.br>
2020-03-15 18:22:32 -0300

    Add metadata em um arquivo config

--- btcsite/gatsby-config.js
+++ btcsite/gatsby-config.js
@@ -1,9 +1,13 @@
 /**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+* Configure your Gatsby site with this file.
+*
+* See: https://www.gatsbyjs.org/docs/gatsby-config/
+*/
+
+const siteMetadata = require('./config/metadata')
 
 module.exports = {
-  /* Your site config here */
+    siteMetadata,
+    //pathPrefix: '/blog' // Variavel onde podemos colocar todo nosso site dentro desse caminho
+    /* Your site config here */
 }
